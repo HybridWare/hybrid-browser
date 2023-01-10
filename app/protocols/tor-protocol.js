467f84aadc04d8af26690fb5a03e1690652677f7
ducksandgoats <mrtraderjoseph@gmail.com>
2023-01-09 21:45:02 -0600

    use new handlers

--- app/protocols/tor-protocol.js
+++ app/protocols/tor-protocol.js
@@ -3,7 +3,7 @@ const fetchToHandler = require('./fetch-to-handler')
 module.exports = async function createHandler (options, session) {
   const makeFetch = require('onion-fetch')
 
-  const fetch = makeFetch(options)
+  const fetch = await makeFetch(options)
 
   return fetchToHandler(fetch, session)
 }
