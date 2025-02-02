76975dd03e8ca19c71eac8c5c1b1f192273016fd
ducksandgoats <mrtraderjoseph@gmail.com>
2025-02-02 16:27:33 -0600

    update

--- app/version.js
+++ app/version.js
@@ -1,21 +1,38 @@
 
-export const name = 'agregore-browser'
-export const version = '2.7.0'
+export const name = 'hybrid-browser'
+export const version = '3.0.4'
 export const dependencies = {
+  "@chainsafe/libp2p-gossipsub": "^14.1.0",
+  "@derhuerst/gemini": "^2.0.0",
+  "@helia/unixfs": "^4.0.2",
+  "@libp2p/identify": "^3.0.15",
+  "@libp2p/kad-dht": "^14.2.0",
   "abort-controller": "^3.0.0",
-  "create-desktop-shortcuts": "^1.7.0",
-  "data-uri-to-buffer": "^3.0.1",
+  "blockstore-fs": "^2.0.2",
+  "create-desktop-shortcuts": "^1.11.0",
+  "data-uri-to-buffer": "^6.0.2",
+  "datastore-fs": "^10.0.2",
   "decompress": "^4.2.1",
   "delay": "^6.0.0",
-  "fs-extra": "^9.0.1",
-  "gemini-to-html": "^1.0.0",
-  "hyper-sdk": "^4.5.1",
-  "mime": "^2.4.6",
+  "detect-port": "^2.1.0",
+  "electron-extended-webextensions": "github:HybridWare/electron-extended-WebExtensions",
+  "event-iterator": "^2.0.0",
+  "fs-extra": "^11.3.0",
+  "gopher-lib": "^0.2.0",
+  "helia": "^5.2.0",
+  "http-proxy-agent": "^7.0.2",
+  "hyper-sdk": "^5.1.0",
+  "mime": "^4.0.6",
   "multiformats": "^9.9.0",
-  "p-queue": "^7.3.4",
+  "node-fetch": "^3.3.2",
+  "p-queue": "^8.1.0",
+  "range-parser": "^1.2.1",
   "rc": "^1.2.8",
   "sanitize-filename": "^1.6.3",
   "scoped-fs": "^1.4.1",
-  "semver": "^7.5.2",
-  "whatwg-mimetype": "https://github.com/jsdom/whatwg-mimetype#v2.3.0"
+  "semver": "^7.7.0",
+  "socks-proxy-agent": "^8.0.5",
+  "streamx": "^2.22.0",
+  "torrentz": "^18.0.7",
+  "whatwg-mimetype": "github:jsdom/whatwg-mimetype"
 }
