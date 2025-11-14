dcba41dd6b66d39abb6d7fe260586efa3758baae
ducksandgoats <mrtraderjoseph@gmail.com>
2025-11-13 19:43:36 -0600

    add orbitdb and hyperbee

--- app/protocols/hyperdb-protocol.js
+++ app/protocols/hyperdb-protocol.js
@@ -5,7 +5,6 @@ export default async function makeHYPERDBFetch (opts = {}) {
     const { Readable, pipelinePromise } = await import('streamx')
     const fse = await import('fs-extra')
     const crypto = await import('crypto')
-    const hostType = '_'
     const mainHeaders = {
       'Access-Control-Allow-Origin': '*',
       'Allow-CSP-From': '*',
@@ -34,9 +33,9 @@ export default async function makeHYPERDBFetch (opts = {}) {
       const core = await sdk.get(title)
       const db = new Hyperbee(core, { keyEncoding: 'utf-8', valueEncoding: 'json' })
       await db.ready()
-      const str = db.key.toString('hex')
-      sdk.join(str, {})
-      addrs.set(str, db)
+      db.strForAddress = db.key.toString('hex')
+      sdk.join(db.strForAddress, {})
+      addrs.set(db.strForAddress, db)
       return db
     }
 
@@ -70,8 +69,7 @@ export default async function makeHYPERDBFetch (opts = {}) {
 
       if(method === 'GET'){
           const useOpt = reqHeaders.has('x-opt') || searchParams.has('x-opt') ? JSON.parse(reqHeaders.get('x-opt') || decodeURIComponent(searchParams.get('x-opt'))) : {}
-          const test = main.useHost === hostType ? reqHeaders.get('x-address') || searchParams.get('x-address') : main.useHost
-          const db = addrs.has(test) ? addrs.get(test) : await startTitle(test)
+          const db = addrs.has(main.useHost) ? addrs.get(main.useHost) : await startTitle(main.useHost)
           const fullPath = main.usePath.slice(1)
 
           if(fullPath){
@@ -82,11 +80,10 @@ export default async function makeHYPERDBFetch (opts = {}) {
             for await (const record of db.createReadStream(useOpt)){
               arr.push(record.value)
             }
-            return new Response(JSON.stringify(arr), {status: 200, headers: {...mainHeaders, 'Content-Type': 'application/json; charset=UTF-8'}})
+            return new Response(JSON.stringify(arr), {status: 200, headers: {...mainHeaders, 'X-Address': db.strForAddress, 'Content-Type': 'application/json; charset=UTF-8'}})
           }
       } else if(method === 'POST'){
-        const test = main.useHost === hostType ? reqHeaders.get('x-address') || searchParams.get('x-address') : main.useHost
-          const db = addrs.has(test) ? addrs.get(test) : await startTitle(test)
+          const db = addrs.has(main.useHost) ? addrs.get(main.useHost) : await startTitle(main.useHost)
           const fullPath = main.usePath.slice(1)
           const getSaved = reqHeaders.has('content-type') ? reqHeaders.get('content-type').includes('multipart/form-data') ? Object.fromEntries((await session.formData()).entries()) : JSON.parse(await streamToString(body)) : JSON.parse(await streamToString(body))
           const stamp = Date.now()
@@ -94,17 +91,17 @@ export default async function makeHYPERDBFetch (opts = {}) {
           getSaved.stamp = getSaved.stamp || stamp
           const prop = getSaved._id
           await db.put(prop, getSaved)
-          return new Response(JSON.stringify(prop), {status: 200, headers: {...mainHeaders, 'X-Address': db.key.toString('hex'), 'Content-Type': 'application/json; charset=UTF-8'}})
+          return new Response(JSON.stringify(prop), {status: 200, headers: {...mainHeaders, 'X-Address': db.strForAddress, 'Content-Type': 'application/json; charset=UTF-8'}})
       } else if(method === 'DELETE'){
-        const test = main.useHost === hostType ? reqHeaders.get('x-address') || searchParams.get('x-address') : main.useHost
-          const db = addrs.has(test) ? addrs.get(test) : await startTitle(test)
+          const db = addrs.has(main.useHost) ? addrs.get(main.useHost) : await startTitle(main.useHost)
+          const strToUse = db.strForAddress
           const fullPath = main.usePath.slice(1)
           if(fullPath){
             await db.del(fullPath)
           } else {
-            await stopTitle(test, db)
+            await stopTitle(strToUse, db)
           }
-          return new Response(JSON.stringify(fullPath || test), { status: 200, headers: { ...mainHeaders, 'Content-Type': 'application/json; charset=UTF-8' } })
+          return new Response(JSON.stringify(fullPath || test), { status: 200, headers: { ...mainHeaders, 'X-Address': strToUse, 'Content-Type': 'application/json; charset=UTF-8' } })
       } else {
         return new Response('invalid method', {status: 400, headers: mainHeaders})
       }
--- app/protocols/ipfsdb-protocol.js
+++ app/protocols/ipfsdb-protocol.js
@@ -3,9 +3,10 @@ export default async function makeIPFSDBFetch (opts = {}) {
   const repo = opts.repo
   const fse = await import('fs-extra')
   const { createOrbitDB } = await import('@orbitdb/core')
+  const {CID} = await import('multiformats')
+  const {base58btc} = (await import('multiformats/basics')).bases
   const crypto = await import('crypto')
   const orbitdb = await createOrbitDB({ ipfs: opts.helia })
-  const hostType = '.'
     const { Readable } = await import('streamx')
     const mainHeaders = {
       'Access-Control-Allow-Origin': '*',
@@ -35,10 +36,18 @@ export default async function makeIPFSDBFetch (opts = {}) {
     }
 
     async function startTitle(title){
-      const useTitle = /^[A-HJ-NP-Za-km-z1-9]*$/.test(title) ? `/orbitdb/${title}` : title
+      let titleToUse
+      try {
+        titleToUse = CID.parse(title).toString(base58btc.encoder)
+      } catch {
+        titleToUse = title
+      }
+      const useTitle = /^[A-HJ-NP-Za-km-z1-9]*$/.test(titleToUse) ? `/orbitdb/${titleToUse}` : titleToUse
       const test = await orbitdb.open(useTitle, {type: 'documents'})
       const str = test.address.split('/').filter(Boolean)[1]
-      addrs.set(str, test)
+      const useStr = CID.parse(str, base58btc.decoder).toV1().toString()
+      test.strForAddress = useStr
+      addrs.set(test.strForAddress, test)
       return test
     }
 
@@ -59,39 +68,37 @@ export default async function makeIPFSDBFetch (opts = {}) {
 
       if(method === 'GET'){
         const useOpt = reqHeaders.has('x-opt') || searchParams.has('x-opt') ? JSON.parse(reqHeaders.get('x-opt') || decodeURIComponent(searchParams.get('x-opt'))) : {}
-        const test = useHost === hostType ? reqHeaders.get('x-address') || searchParams.get('x-address') : useHost
-        const odb = addrs.has(test) ? addrs.get(test) : await startTitle(test)
+        const odb = addrs.has(useHost) ? addrs.get(useHost) : await startTitle(useHost)
         const usePath = fullPath.slice(1)
         if(usePath){
           const useData = await odb.get(usePath)
-          return new Response(JSON.stringify(useData.value), {status: 200, headers: {...mainHeaders, 'Content-Type': 'application/json; charset=UTF-8'}})
+          return new Response(JSON.stringify(useData.value), {status: 200, headers: {...mainHeaders, 'X-Address': odb.strForAddress, 'Content-Type': 'application/json; charset=UTF-8'}})
         } else {
           const arr = []
           for await (const record of odb.iter(useOpt)){
             arr.push(record.value)
           }
-          return new Response(JSON.stringify(arr), {status: 200, headers: {...mainHeaders, 'Content-Type': 'application/json; charset=UTF-8'}})
+          return new Response(JSON.stringify(arr), {status: 200, headers: {...mainHeaders, 'X-Address': odb.strForAddress, 'Content-Type': 'application/json; charset=UTF-8'}})
         }
       } else if(method === 'POST'){
-        const test = useHost === hostType ? reqHeaders.get('x-address') || searchParams.get('x-address') : useHost
-        const odb = addrs.has(test) ? addrs.get(test) : await startTitle(test)
+        const odb = addrs.has(useHost) ? addrs.get(useHost) : await startTitle(useHost)
         const usePath = fullPath.slice(1)
         const getSaved = reqHeaders.has('content-type') ? reqHeaders.get('content-type').includes('multipart/form-data') ? Object.fromEntries((await session.formData()).entries()) : JSON.parse(await streamToString(body)) : JSON.parse(await streamToString(body))
         const stamp = Date.now()
         getSaved._id = usePath || getSaved._id || stamp + '-' + crypto.createHash('sha256').update(crypto.randomUUID()).digest('hex')
         getSaved.stamp = getSaved.stamp || stamp
         const hash = await odb.put(getSaved)
-        return new Response(JSON.stringify(getSaved._id), {status: 200, headers: {...mainHeaders, 'X-Hash': hash, 'X-Address': odb.address.split('/').filter(Boolean)[1], 'Content-Type': 'application/json; charset=UTF-8'}})
+        return new Response(JSON.stringify(getSaved._id), {status: 200, headers: {...mainHeaders, 'X-Hash': hash, 'X-Address': odb.strForAddress, 'Content-Type': 'application/json; charset=UTF-8'}})
       } else if(method === 'DELETE'){
-        const test = useHost === hostType ? reqHeaders.get('x-address') || searchParams.get('x-address') : useHost
-        const odb = addrs.has(test) ? addrs.get(test) : await startTitle(test)
+        const odb = addrs.has(useHost) ? addrs.get(useHost) : await startTitle(useHost)
+        const strToUse = odb.strForAddress
         const usePath = fullPath.slice(1)
         if(usePath){
           await odb.del(usePath)
         } else {
-          await stopTitle(test, odb)
+          await stopTitle(strToUse, odb)
         }
-        return new Response(JSON.stringify(usePath || useHost), { status: 200, headers: { ...mainHeaders, 'Content-Type': 'application/json; charset=UTF-8' } })
+        return new Response(JSON.stringify(usePath || useHost), { status: 200, headers: { ...mainHeaders, 'X-Address': strToUse, 'Content-Type': 'application/json; charset=UTF-8' } })
       } else {
         return new Response('invalid method', {status: 400, headers: mainHeaders})
       }
